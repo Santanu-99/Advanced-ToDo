5de2094a5d180b409b6dcb7180c8db58806dca68
Santanu-99 <bitup62@gmail.com>
2022-03-24 00:05:46 +0530

    Designing updated

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let addBtn = document.querySelector('.add-btn');
-
+let modalCont = document.querySelector('.modal-cont');
+let modalContShow = false;
 addBtn.addEventListener('click',function(e){
     // Display a model
+    modalContShow = !modalContShow;
+    if(modalContShow){
+        modalCont.style.display = "flex";
+    }
+    else{
+        modalCont.style.display = "none";
+    }
 
     // Create a card accordingly and add it
 })
